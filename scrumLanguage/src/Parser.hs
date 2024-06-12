b5800c82b1384101892a4e5bf172ada81ea3952a
SamuelEscalera <samuel.escalera.9413603@usalesiana.edu.bo>
2024-06-12 03:05:54 -0400

    parseConditional implementation

--- scrumLanguage/src/Parser.hs
+++ scrumLanguage/src/Parser.hs
@@ -32,13 +32,36 @@ parseProgram :: Parser [Expression]
 parseProgram = sepBy parseExpression (many1 space <|> many1 newline)
 
 
-parserComparison :: Parser Comparison
-parserComparison = Comp <$> parseExpression <*> parserCompOperator <*> parseExpression
+parseComparison :: Parser Comparison
+parseComparison = Comp <$> parseExpression <*> parseCompOperator <*> parseExpression
 
-parserCompOperator :: Parser CompOperator
-parserCompOperator = (Equal <$ string "==")
+parseCompOperator :: Parser CompOperator
+parseCompOperator = (Equal <$ string "==")
                   <|>(NotEqual <$ string "/=")
                   <|>(Less <$ string "<")
                   <|>(LessEqual <$ string "<=")
                   <|>(Greater <$ string ">")
                   <|>(GreaterEqual <$ string ">=")
+
+{-
+parseConditional :: Parser Expression
+parseConditional = do
+  _ <- string "if" *> spaces *> char '('
+  condition <- parseComparison
+  _ <- char ')' *> spaces *> char '{'
+  ifExpr <- parseExpression
+  _ <- char '}' *> spaces *> string "else" *> spaces *> char '{'
+  elseExpr <- parseExpression
+  _ <- char '}'
+  return $ Conditional condition ifExpr elseExpr
+-}
+parseConditional :: Parser Expression
+parseConditional =
+    string "if" *> spaces *> char '(' *>
+    parseComparison >>= \condition ->
+    char ')' *> spaces *> char '{' *>
+    parseExpression >>= \ifExpr ->
+    char '}' *> spaces *> string "else" *> spaces *> char '{' *>
+    parseExpression >>= \elseExpr ->
+    char '}' *> 
+    pure (Conditional condition ifExpr elseExpr)
